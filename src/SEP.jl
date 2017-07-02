aca0f8d1bd4f5a794b62821a407deed59fedc14b
Kyle Barbary <kylebarbary@gmail.com>
2017-07-01 17:54:02 -0700

    started implementing extract()

--- src/SEP.jl
+++ src/SEP.jl
@@ -1,7 +1,7 @@
 module SEP
 
-import Base: collect, -
-export Background, background
+import Base: collect, broadcast!, -, show
+export Background, background, rms
 
 if isfile(joinpath(dirname(@__FILE__),"..","deps","deps.jl"))
     include("../deps/deps.jl")
@@ -10,7 +10,8 @@ else
 end
 
 # Julia Type -> numeric type code from sep.h
-const SEPImageType = Union{Bool, UInt8, Cint, Cfloat, Cdouble}
+const SEPMaskType = Union{Bool, UInt8, Cint, Cfloat, Cdouble}
+const PixType = Union{UInt8, Cint, Cfloat, Cdouble}
 sep_typecode(::Type{Bool}) = Cint(11)
 sep_typecode(::Type{UInt8}) = Cint(11)
 sep_typecode(::Type{Cint}) = Cint(31)
@@ -19,9 +20,15 @@ sep_typecode(::Type{Cdouble}) = Cint(82)
 
 
 # definitions from sep.h
-const SEP_NOISE_NONE = Cint(0)
-const SEP_NOISE_STDDEV = Cint(1)
-const SEP_NOISE_VAR = Cint(2)
+const SEP_NOISE_NONE = Cshort(0)
+const SEP_NOISE_STDDEV = Cshort(1)
+const SEP_NOISE_VAR = Cshort(2)
+
+const SEP_THRESH_REL = Cint(0)
+const SEP_THRESH_ABS = Cint(1)
+
+const SEP_FILTER_CONV = Cint(0)
+const SEP_FILTER_MATCHED = Cint(1)
 
 function sep_assert_ok(status::Cint)
     if status != 0
@@ -32,7 +39,7 @@ function sep_assert_ok(status::Cint)
     end
 end
 
-# internal use only.
+# internal use only: mirrors `sep_image` struct in sep.h
 struct sep_image
 data::Ptr{Void}
 noise::Ptr{Void}
@@ -48,11 +55,61 @@ gain::Cdouble
 maskthresh::Cdouble
 end
 
+function sep_image(data::Array{T, 2};
+                   noise=nothing, mask=nothing, noise_type=:stddev,
+                   gain=0.0, mask_thresh=0.0) where {T<:PixType}
+    sz = size(data)
 
+    # data is required
+    data_ptr = Ptr{Void}(pointer(data))
+    dtype = sep_typecode(T)
+
+    # mask options
+    mask_ptr = C_NULL
+    mdtype = Cint(0)
+    if mask !== nothing
+        isa(mask, Matrix) || error("mask must be a 2-d array")
+        size(mask) == sz || error("data and mask must be same size")
+        mask_ptr = Ptr{Void}(pointer(mask))
+        mdtype = sep_typecode(eltype(mask))
+    end
+
+    # noise options
+    ndtype = Cint(0)
+    noise_ptr = C_NULL
+    noiseval = 0.0
+    noise_typecode = SEP_NOISE_NONE
+    if noise !== nothing
+        if isa(noise, Matrix)
+            size(noise) == sz || error("noise array must be same size as data")
+            noise_ptr = sep_typecode(eltype(noise))
+            ndtype = sep_typecode(eltype(ndtype))
+        elseif isa(noise, Number)
+            noiseval = Cdouble(noise)
+        else
+            error("noise must be array or number")
+        end
+        noise_typecode = ((noise_type == :stddev) ? SEP_NOISE_STDDEV :
+                          (noise_type == :var) ? SEP_NOISE_VAR :
+                          error("noise_type must be :stddev or :var"))
+    end
+            
+    return sep_image(data_ptr, noise_ptr, mask_ptr,
+                     dtype, ndtype, mdtype,
+                     sz[1], sz[2],
+                     noiseval, noise_typecode,
+                     Cdouble(gain),
+                     Cdouble(maskthresh))
+end
 
 # ---------------------------------------------------------------------------
 # Background functions
 
+"""
+    Background
+
+
+"""
 mutable struct Background
     ptr::Ptr{Void}
     data_size::Tuple{Int, Int}
@@ -61,195 +118,180 @@ end
 free!(bkg::Background) = ccall((:sep_bkg_free, libsep), Void, (Ptr{Void},),
                                bkg.ptr)
 
+global_mean(bkg::Background) = ccall((:sep_bkg_global, libsep), Cfloat,
+                                     (Ptr{Void},), bkg.ptr)
+
+global_rms(bkg::Background) = ccall((:sep_bkg_globalrms, libsep), Cfloat,
+                                    (Ptr{Void},), bkg.ptr)
+
+function show(io::IO, bkg::Background)
+    print(io, "Background $(bkg.data_size[1])×$(bkg.data_size[2])\n")
+    print(io, " - global mean: $(global_mean(bkg))\n")
+    print(io, " - global rms : $(global_rms(bkg))\n")
+end
 
 function background(data::Array{T, 2}; mask=nothing, boxsize=(64, 64),
-                    filtersize=(3, 3), filterthresh=0.0, maskthresh=0) where {T<:SEPImageType}
-    sz = size(data)
-    dtype = sep_typecode(T)
-
-    # mask options
-    mdtype = Cint(0)
-    mask_ptr = C_NULL
-    if mask !== nothing
-        isa(mask, Matrix) || error("mask must be a 2-d array")
-        size(mask) == sz || error("data and mask must be same size")
-        mdtype = sep_typecode(T)
-        mask_ptr = Ptr{Void}(pointer(mask))
-    end
-
-    im = sep_image(Ptr{Void}(pointer(data)),
-                   C_NULL,
-                   mask_ptr,
-                   dtype,
-                   Cint(0),
-                   mdtype,
-                   sz[1],
-                   sz[2],
-                   0.0,
-                   SEP_NOISE_NONE,  # SEP_NOISE_NONE
-                   0.0,
-                   Cdouble(maskthresh))
-
+                    filtersize=(3, 3), filterthresh=0.0, mask_thresh=0) where {T<:PixType}
+    im = sep_image(data; mask=mask, mask_thresh=mask_thresh)
     result = Ref{Ptr{Void}}(C_NULL)
     status = ccall((:sep_background, libsep), Cint,
                    (Ptr{sep_image}, Cint, Cint, Cint, Cint, Cdouble,
                     Ref{Ptr{Void}}),
-                   &im, boxsize[1], boxsize[2],
-                   filtersize[1], filtersize[2], filterthresh, result)
+                   &im, boxsize[1], boxsize[2], filtersize[1], filtersize[2],
+                   filterthresh, result)
     sep_assert_ok(status)
-
     bkg = Background(result[], sz)
     finalizer(bkg, free!)
     return bkg
 end
 
-#=
-function collect{T}(bkgmap::Background{T})
-    result = Array(T, bkgmap.data_size)
-    status = ccall((:sep_backarray, libsep), Cint, (Ptr{Void}, Ptr{Void}, Cint),
-                   bkgmap.ptr, result, sep_typecode(T))
+
+function collect(::Type{T}, bkg::Background) where {T<:PixType}
+    result = Array{T}(bkg.data_size)
+    status = ccall((:sep_bkg_array, libsep), Cint, (Ptr{Void}, Ptr{Void}, Cint),
+                   bkg.ptr, result, sep_typecode(T))
     sep_assert_ok(status)
     return result
 end
+# default collection type is Float32, because that's what's natively stored in
+# background.
+# TODO: make default the input array type in `background` instead?
+collect(bkg::Background) = collect(Float32, bkg)
+
 
 """
-subtract!(bkgmap, A)
+    rms(bkg)
+    rms(T, bkg)
 
-Subtract a background from an array in-place.
+Return an array of the standard deviation of the background. The result is
+the size of the original image and is of type `T`, if given.
 """
-function subtract!{T}(bkgmap::Background, A::Array{T, 2})
-    status = ccall((:sep_subbackarray, libsep), Cint,
+function rms(::Type{T}, bkg::Background) where {T<:PixType}
+    result = Array{T}(bkg.data_size)
+    status = ccall((:sep_bkg_rmsarray, libsep), Cint,
                    (Ptr{Void}, Ptr{Void}, Cint),
-                   bkgmap.ptr, A, sep_typecode(T))
+                   bkg.ptr, result, sep_typecode(T))
+    sep_assert_ok(status)
+    return result
+end
+rms(bkg::Background) = rms(Float32, bkg)
+
+
+# In-place background subtraction: A .-= bkg
+function broadcast!(-, A::Array{T, 2},  ::Array{T, 2}, bkg::Background) where {T<:PixType}
+    if size(A) != bkg.data_size
+         throw(DimensionMismatch("dimensions must match"))
+    end
+    status = ccall((:sep_bkg_subarray, libsep), Cint,
+                   (Ptr{Void}, Ptr{Void}, Cint),
+                   bkg.ptr, A, sep_typecode(T))
     sep_assert_ok(status)
 end
 
-function (-){T}(A::Array{T, 2}, bkgmap::Background)
+function (-)(A::Array{T, 2}, bkg::Background) where {T<:PixType}
     B = copy(A)
-    subtract!(bkgmap, B)
+    B .-= bkg
     return B
 end
 
-=#
 
 # ---------------------------------------------------------------------------
 # Source Extraction
 
-#=
+
 # Mirror of C struct
-immutable sepobj
-    thresh::Cfloat
-    npix::Cint
-    tnpix::Cint
-    xmin::Cint
-    xmax::Cint
-    ymin::Cint
-    ymax::Cint
-    mx::Cdouble
-    my::Cdouble
-    mx2::Cdouble
-    my2::Cdouble
-    mxy::Cdouble
-    a::Cfloat
-    b::Cfloat
-    theta::Cfloat
-    abcor::Cfloat
-    cxx::Cfloat
-    cyy::Cfloat
-    cxy::Cfloat
-    cflux::Cfloat
-    flux::Cfloat
-    cpeak::Cfloat
-    peak::Cfloat
-    flag::Cshort
-    pix::Ptr{Cint}
+struct sep_catalog
+    nobj::Cint                 # number of objects (length of all arrays)
+    thresh::Ptr{Cfloat}              # threshold (ADU)
+    npix::Ptr{Cint}              # # pixels extracted (size of pix array)
+    tnpix::Ptr{Cint}                # # pixels above thresh (unconvolved)
+    xmin::Ptr{Cint}
+    xmax::Ptr{Cint}
+    ymin::Ptr{Cint}
+    ymax::Ptr{Cint}
+    x::Ptr{Cdouble}              # barycenter (first moments)
+    y::Ptr{Cdouble}
+    x2::Ptr{Cdouble}     # second moments
+    y2::Ptr{Cdouble}
+    xy::Ptr{Cdouble}
+    errx2::Ptr{Cdouble}  # second moment errors
+    erry2::Ptr{Cdouble}
+    errxy::Ptr{Cdouble}
+    a::Ptr{Cfloat}       # ellipse parameters
+    b::Ptr{Cfloat}
+    theta::Ptr{Cfloat}
+    cxx::Ptr{Cfloat}        # alternative ellipse parameters
+    cyy::Ptr{Cfloat}
+    cxy::Ptr{Cfloat}
+    cflux::Ptr{Cfloat}   # total flux of pixels (convolved)
+    flux::Ptr{Cfloat}    # total flux of pixels (unconvolved)
+    cpeak::Ptr{Cfloat}   # peak pixel flux (convolved)
+    peak::Ptr{Cfloat}    # peak pixel flux (unconvolved)
+    xcpeak::Ptr{Cint}    # x, y coords of peak (convolved) pixel
+    ycpeak::Ptr{Cint}
+    xpeak::Ptr{Cint}     # x, y coords of peak (unconvolved) pixel
+    ypeak::Ptr{Cint}
+    flag::Ptr{Cshort}    # extraction flags
+    pix::Ptr{Ptr{Cint}}  # array giving indicies of object's pixels in
+                         # image (linearly indexed). Length is `npix`.
+                         # (pointer to within the `objectspix` buffer)
+    objectspix::Ptr{Cint}  # buffer holding pixel indicies for all objects
 end
 
-type AstroSource
-    thresh::Cfloat
-    npix::Cint
-    tnpix::Cint
-    xmin::Cint
-    xmax::Cint
-    ymin::Cint
-    ymax::Cint
-    mx::Cdouble
-    my::Cdouble
-    mx2::Cdouble
-    my2::Cdouble
-    mxy::Cdouble
-    a::Cfloat
-    b::Cfloat
-    theta::Cfloat
-    abcor::Cfloat
-    cxx::Cfloat
-    cyy::Cfloat
-    cxy::Cfloat
-    cflux::Cfloat
-    flux::Cfloat
-    cpeak::Cfloat
-    peak::Cfloat
-    flag::Cshort
-    pix::Array{Cint, 1}
-end
 
-function extractobjs(im::Array{Float32, 2}, thresh::Real)
-    nx, ny = size(im)
-    conv = Float32[1 2 1; 2 4 2; 1 2 1]
-    nobj = Array(Int32, 1)
-    ptr = Array(Ptr{sepobj}, 1)
-    status = ccall((:extractobj, libsep), Cint,
-                   (Ptr{Cfloat}, Ptr{Cfloat}, Cint, Cint, Cfloat, Cint,
-                    Ptr{Cfloat}, Cint, Cint,
-                    Cint, Cdouble, Cint, Cdouble,
-                    Ptr{Cint}, Ptr{Ptr{sepobj}}),
-                   im, C_NULL, nx, ny, thresh, 5,
-                   conv, size(conv, 1), size(conv, 2),
-                   32, 0.005, 1, 1.0,
-                   nobj, ptr)
+function extract(data::Array{T, 2}, thresh::Real;
+                 noise=nothing, mask=nothing, noise_type=:stddev,
+                 thresh_type=:relative, minarea=5,
+                 filter_kernel=Float32[1 2 1; 2 4 2; 1 2 1],
+                 filter_type=:matched,
+                 deblend_nthresh=32, deblend_cont=0.005,
+                 clean=true, clean_param=1.0,
+                 gain=0.0, mask_thresh=0) where {T}
 
-    n = int64(nobj[1])
+    im = sep_image(data; noise=noise, mask=mask, noise_type=noise_type,
+                   gain=gain, mask_thresh=mask_thresh)
 
-    # not taking ownership... need to free at the end.
-    cresult = pointer_to_array(ptr[1], n)
+    thresh_typecode = ((thresh_type == :relative)? SEP_THRESH_REL :
+                       (thresh_type == :absolute)? SEP_THRESH_ABS :
+                       error("thresh_type must be :relative or :absolute"))
+
+    filter_typecode = ((filter_type == :matched)? SEP_FILTER_MATCHED :
+                       (filter_type == :convolution)? SEP_FILTER_CONV :
+                       error("filter_type must be :matched or :convolution"))
+
+    # convert filter kernel to Cfloat array
+    filter_kernel_cfloat = convert(Array{Cfloat, 2}, filter_kernel)
+    filter_size = size(filter_kernel_cfloat)
     
-    result = Array(AstroSource, n)
-    for i=1:n
-        result[i] = AstroSource(cresult[i].thresh,
-                                cresult[i].npix,
-                                cresult[i].tnpix,
-                                cresult[i].xmin,
-                                cresult[i].xmax,
-                                cresult[i].ymin,
-                                cresult[i].ymax,
-                                cresult[i].mx,
-                                cresult[i].my,
-                                cresult[i].mx2,
-                                cresult[i].my2,
-                                cresult[i].mxy,
-                                cresult[i].a,
-                                cresult[i].b,
-                                cresult[i].theta,
-                                cresult[i].abcor,
-                                cresult[i].cxx,
-                                cresult[i].cyy,
-                                cresult[i].cxy,
-                                cresult[i].cflux,
-                                cresult[i].flux,
-                                cresult[i].cpeak,
-                                cresult[i].peak,
-                                cresult[i].flag,
-                                pointer_to_array(cresult[i].pix,
-                                                 (int(cresult[i].npix),),
-                                                 true))
-    end
+    result = Ref{Ptr{sep_catalog}}(C_NULL)
+    status = ccall((:sep_extract, libsep), Cint,
+                   (Ptr{sep_image},
+                    Cfloat, Cint, # thresh, thresh_type
+                    Cint,  # minarea  
+                    Ptr{Cfloat}, Cint, Cint,  # conv, convw, convh
+                    Cint, # filter_type
+                    Cint, Cdouble,  # deblend_nthresh, deblend_cont
+                    Cint, Cdouble,  # clean_flag, clean_param
+                    Ref{Ptr{sep_catalog}}),
+                   &im,
+                   thresh, thresh_typecode,
+                   minarea,
+                   filter_kernel_cfloat, filter_size[1], filter_size[2],
+                   filter_typecode,
+                   deblend_nthresh, deblend_cont,
+                   clean_flag, clean_param,
+                   result)
+    sep_assert_ok(status)
 
-    c_free(ptr[1])  # Cannot access cresult after this!
+    # parse result
+    catalog_ptr = result[]
+    # ...
     
-    result
+    # free result allocated in sep_extract
+    ccall((:sep_catalog_free, libsep), Void, (Ptr{sep_catalog},), catalog_ptr)
+
+    # todo: return actual result
+    nothing
 end
-
-=#
-
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,33 @@
 using SEP
 using Base.Test
-
-
 using FITSIO
 
-# use test image in deps directory
-image_name = joinpath(dirname(@__FILE__), "..", "deps", "src", "sep-1.0.0",
-                      "data", "image.fits")
-f = FITS(image_name)
-data = read(f[1])
 
+# use test image in deps directory
+datadir = joinpath(dirname(@__FILE__), "..", "deps", "src", "sep-1.0.0",
+                   "data")
+
+data = read(FITS(joinpath(datadir, "image.fits"))[1])
+back_sextractor = read(FITS(joinpath(datadir, "back.fits"))[1])
+rms_sextractor = read(FITS(joinpath(datadir, "rms.fits"))[1])
+
+# test background
 bkg = background(data)
 println(bkg)
-#subtract!(data, bkg)
-#print(data)
+
+back = collect(bkg)
+println(back ≈ back_sextractor)
+
+for T in (Float32, Float64)
+    A = zeros(T, size(data))
+    back = collect(T, bkg)
+    println(back ≈ back_sextractor)
+end
+
+A = zeros(Float32, size(data))
+A .-= bkg
+
+B = zeros(Float32, size(data))
+B -= bkg
+@test A ≈ B
+
